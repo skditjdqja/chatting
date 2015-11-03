a7544bed312684bd7268ba09b33994b763346b09
lazaroalvin <lazaroalvin@rocketmail.com>
2015-11-03 11:51:00 +0700

    Changes to be committed: 	modified:   Server_Simple.py

--- Server_Simple.py
+++ Server_Simple.py
@@ -1,12 +1,11 @@
-import sys
-import socket
+import sys, socket
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SQL_SOCKET, socket.SO_REUSEADDR,1)
 
 # Bind the socket to the port
-server_address = ('', 10000)
+server_address = ('', 8888)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
